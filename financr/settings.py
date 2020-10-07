605c2560f103ebf72c73d3811e07e3f063fc991c
bil-bal <bilal.balguen@gmx.de>
2020-10-07 12:38:40 +0200

    Update settings.py

--- financr/settings.py
+++ financr/settings.py
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'de-De'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -126,4 +126,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-LOGIN_URL = 'home'
+LOGIN_URL = 'start'
